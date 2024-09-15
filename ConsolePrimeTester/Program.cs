06fbe057548722f6c09bf3e0114eb5509e17dca0
Bart <bart.vanderwal@han.nl>
2024-09-15 11:36:43 +0200

    PrimeChecker naar aparte Class Library, en in Main argument uitlezen inclusief fout afhandeling.

--- ConsolePrimeTester/Program.cs
+++ ConsolePrimeTester/Program.cs
@@ -1,4 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Zie de README.md voor meer informatie.
+namespace ConsolePrimeTester
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int priemKandidaat = 0;
+            if (args.Length >= 2)
+            {
+                throw new ArgumentException("Je moet precies 1 argument geven, niet " + args.Length+ " argumenten");
+            }
+            if (args.Length==0)
+            {
+                priemKandidaat = new Random(0).Next();
+                Console.WriteLine("Geen getal meegegeven, we gebruiken random getal '" + priemKandidaat + "'.");
+            }
+            else
+            {
+                if (!Int32.TryParse(args[0], out priemKandidaat))
+                {
+                    throw new ArgumentException("Input " + args[0] + " is geen geldig integer getal.");
+                }
+            }
 
-var primeTester = new PriemgetalChecker();
-primeTester.testPrime(2);
+            var primeTester = new PriemgetalChecker();
+            var isPriem = primeTester.testPrime(priemKandidaat);
+            Console.WriteLine("Is getal " + priemKandidaat + " een priemgetal? " + (isPriem ? "JA" : "NEE"));
+        }
+    }
+}
