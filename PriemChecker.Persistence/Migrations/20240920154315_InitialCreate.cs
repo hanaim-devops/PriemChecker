8d5e6fec6c1c32595f5b2ba31b85dd8867ee957c
Bart <bart.vanderwal@han.nl>
2024-09-20 17:46:11 +0200

    Nog de migrations bestanden toegevoegd. https://chatgpt.com/share/66e9f021-d8a8-8012-a6dc-a4ad86f3f3b6

---
+++ PriemChecker.Persistence/Migrations/20240920154315_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriemChecker.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PriemCheckResultaten",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PriemKandidaatWaarde = table.Column<int>(type: "int", nullable: false),
+                    IsPriemgetal = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriemCheckResultaten", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PriemCheckResultaten");
+        }
+    }
+}
